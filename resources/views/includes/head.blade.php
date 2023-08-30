cd6cff2c45701b7c23b3b9a3291163327a71c628
Mahaveer1saini <143497823+Mahaveer1saini@users.noreply.github.com>
2023-08-30 16:25:44 +0530

    u

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -12,7 +12,7 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
-    
+
     <!-- awesome  -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
@@ -24,7 +24,7 @@
 
 
 
-<style> 
+<style>
     @import url('https://fonts.googleapis.com/css?family=Poppins:400,500,600,700,800,900');
 
     body {
@@ -329,9 +329,6 @@
     box-shadow: 0 8px 24px 0 rgba(16, 39, 112, .2);
     }
 
-
-
-
     .logo {
     position: absolute;
     top: 30px;
@@ -346,4 +343,5 @@
     width: auto;
     display: block;
     }
+
     </style>
